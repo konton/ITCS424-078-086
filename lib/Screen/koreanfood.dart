5c987d55f067f7ca485e34e15abd8e4996b87901
DOMEMY <konton07@hotmail.com>
2022-04-21 21:20:00 +0700

    Updated

---
+++ lib/Screen/koreanfood.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class koreanfood extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Select Korean dishes")
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Korean").snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          return ListView(
+            padding: const EdgeInsets.all(15),
+            children: snapshot.data!.docs.map((document) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: [
+
+                  Expanded(
+                    child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            color: Colors.amber,
+                            child: Column(
+                              children: [
+                                Image.network(document['img'],
+                                    width: 200, height: 200, fit: BoxFit.contain),
+                                Text(
+                                  "Name:" + document['name'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  "Calories:" + document['Calories'].toString(),
+                                  style: TextStyle(color: Colors.white, fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                    ),
+                  )
+                ],
+              );
+
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+
+}
